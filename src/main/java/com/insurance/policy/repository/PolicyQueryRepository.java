8b919570da1ec50efad969db2514683b7d5bdf5b
benhrif <benhrif.oussama@gmail.com>
2024-12-10 13:32:12 +0100

    Update PolicyQueryRepository

--- src/main/java/com/insurance/policy/repository/PolicyQueryRepository.java
+++ src/main/java/com/insurance/policy/repository/PolicyQueryRepository.java
@@ -1,6 +1,7 @@
 package com.insurance.policy.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.insurance.policy.model.Policy;
 
